8c63e11d69f40ebd0f81e218b75760295b4dd9b4
Paula Iuga <paula.iuga@eaudeweb.ro>
2022-12-01 16:40:52 +0200

    added custom user model

--- homeify/admin.py
+++ homeify/admin.py
@@ -1,7 +1,34 @@
 from django.db import models
-#from .modules import Task
-
+from .models import CustomUser
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 # registers modules to admin in server (optional)
 
-#admin.site.register(Task)
+class CustomUserAdmin(UserAdmin):
+    # add user
+    add_fieldsets = (
+        (None, {
+            'fields': ('username', 'password1', 'password2')
+        }),
+        ('Personal info', {
+            'fields': ('first_name', 'last_name', 'email', 'rewards', 'image_url')
+        })
+        )
+
+    # view/edit user
+    fieldsets = (
+        (None, {
+            'fields': ('username', 'password1', 'password2')
+        }),
+        ('Personal info', {
+            'fields': ('first_name', 'last_name', 'email', 'rewards', 'image_url')
+        })
+        )
+
+    # list view
+    list_display = (
+        'username', 'email', 'first_name', 'last_name', 'is_staff',
+        'rewards'
+        )
+admin.site.register(CustomUser, CustomUserAdmin)
--- homeify/models.py
+++ homeify/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # holds definitions for our modules (classes)
+
+class CustomUser(AbstractUser):
+    # additional fields added to the default user object
+    rewards = models.PositiveIntegerField(default=0)
+    image_url = models.ImageField(default=None)
