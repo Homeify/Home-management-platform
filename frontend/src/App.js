71b6246943cbd056a99ef10a54c173a2388ca390
Diana Elena Vasiliu <diana.vasiliu10@gmail.com>
2022-12-05 14:42:49 +0200

    Added multiple languages support (i18next)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
-import { Button, ButtonGroup } from '@chakra-ui/react';
+import { useTranslation } from 'react-i18next';
+import './i18n/config.js';
+import { Button } from '@chakra-ui/react';
 
 function App() {
+    const { t } = useTranslation();
     return (
         <div className='App'>
-            <Button colorScheme='primary'>Button</Button>
+            <Button colorScheme='primary'>{t('add')}</Button>
         </div>
     );
 }
---
+++ frontend/src/i18n/config.js
@@ -0,0 +1,22 @@
+import i18n from 'i18next';
+import roTranslation from './ro.json';
+import enTranslation from './en.json';
+import { initReactI18next } from 'react-i18next';
+
+export const resources = {
+    ro: {
+        translation: roTranslation,
+    },
+    en: {
+        translation: enTranslation,
+    },
+};
+
+i18n.use(initReactI18next).init({
+    compatibilityJSON: 'v3',
+    lng: 'ro',
+    interpolation: {
+        escapeValue: false, // not needed for react as it escapes by default
+    },
+    resources,
+});
