f9ffe5ed54f1ad470a843eaf3184c8f2595929e0
Diana Elena Vasiliu <diana.vasiliu10@gmail.com>
2022-12-09 21:10:16 +0200

    Fixed responsiveness

--- frontend/src/hooks/useWindowWidth.js
+++ frontend/src/hooks/useWindowWidth.js
@@ -1,15 +1,19 @@
 import { useEffect, useState } from 'react';
-import { windowWidthMobile } from '../utils/constants';
+import { WINDOW_WIDTH_MOBILE, WINDOW_WIDTH_SMALL } from '../utils/constants';
 
 const useWindowWidth = () => {
     const [winWidth, setWinWidth] = useState(window.innerWidth);
     const [isMobile, setIsMobile] = useState(
-        window.innerWidth <= windowWidthMobile
+        window.innerWidth <= WINDOW_WIDTH_MOBILE
+    );
+    const [isSmall, setIsSmall] = useState(
+        window.innerWidth <= WINDOW_WIDTH_SMALL
     );
 
     const handleResize = () => {
         setWinWidth(window.innerWidth);
-        setIsMobile(window.innerWidth <= windowWidthMobile);
+        setIsMobile(window.innerWidth <= WINDOW_WIDTH_MOBILE);
+        setIsSmall(window.innerWidth <= WINDOW_WIDTH_SMALL);
     };
 
     useEffect(() => {
@@ -20,6 +24,7 @@ const useWindowWidth = () => {
     return {
         winWidth,
         isMobile,
+        isSmall,
     };
 };
 
