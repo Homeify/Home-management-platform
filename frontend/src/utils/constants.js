ccc8caaac584636f8348c253bf3e28ba9886975b
Diana Elena Vasiliu <diana.vasiliu10@gmail.com>
2022-12-09 16:14:45 +0200

    Added db user data in header

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,7 @@
 const windowWidthMobile = 767;
 const BASE_URL = 'http://127.0.0.1:8000';
+const LOCAL_STORAGE_KEYS = {
+    AUTH_TOKEN: 'AUTH_TOKEN',
+};
 
-export { windowWidthMobile, BASE_URL };
+export { windowWidthMobile, BASE_URL, LOCAL_STORAGE_KEYS };
