a5b8234e1dd54de0164db3882f33ce6729644e21
Paula Iuga <paula.iuga@eaudeweb.ro>
2022-12-11 14:35:13 +0200

    added tests for users endpoint

--- homeify/tests.py
+++ homeify/tests.py
@@ -1,7 +1,9 @@
 from rest_framework import status
 from rest_framework.reverse import reverse
 from rest_framework.test import APITestCase, APIClient
-from .views import RegisterAPI
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+from .views import RegisterAPI, LogoutAPI
 from rest_framework.test import APIRequestFactory
 
 
@@ -14,8 +16,12 @@ class UsersAPIViewTestCase(APITestCase):
         self.email = "john@snow.com"
         self.password1 = "you_know_nothing"
         self.password2 = "you_know_nothing"
-        self.url = reverse("register")
-        self.view = RegisterAPI.as_view()
+        self.register_url = reverse("register")
+        self.login_url = reverse("token_obtain_pair")
+        self.logout_url = reverse("logout_user")
+        self.register_view = RegisterAPI.as_view()
+        self.login_view = TokenObtainPairView.as_view()
+        self.logout_view = LogoutAPI.as_view()
 
     def post_data(self):
         data = {"username": self.username,
@@ -24,11 +30,10 @@ class UsersAPIViewTestCase(APITestCase):
                 "email": self.email,
                 "password1": "you_know_nothing",
                 "password2": "you_know_nothing",
-                "image_url": None
                 }
 
-        request = self.factory.post(self.url, data)
-        response = self.view(request)
+        request = self.factory.post(self.register_url, data)
+        response = self.register_view(request)
 
         return response
 
@@ -36,5 +41,59 @@ class UsersAPIViewTestCase(APITestCase):
         response = self.post_data()
         self.assertEqual(status.HTTP_201_CREATED, response.status_code)
 
-    # def test_exsting_user(self):
-    #     # @TODO
+    def test_existing_user(self):
+        ok = False
+        try:
+            self.post_data()
+            response = self.post_data()
+            self.assertEqual(status.HTTP_406_NOT_ACCEPTABLE, response.status_code)
+        except Exception:
+            ok = True
+        self.assertTrue(ok)
+
+    def test_invalid_register(self):
+        data = {
+            "username": self.username,
+            "password1": self.password1
+        }
+        request = self.factory.post(self.register_url, data)
+        response = self.register_view(request)
+        self.assertEqual(status.HTTP_406_NOT_ACCEPTABLE, response.status_code)
+
+    def test_login_user(self):
+        response = self.post_data()
+        self.assertEqual(status.HTTP_201_CREATED, response.status_code)
+
+        data = {
+            "username": self.username,
+            "password": self.password1
+        }
+
+        request = self.factory.post(self.login_url, data)
+        response = self.login_view(request)
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        refresh_token = response.data.get('refresh')
+        access_token = response.data.get('access')
+        self.assertTrue(refresh_token is not None, "refresh token not found in body")
+        self.assertTrue(access_token is not None, "access token not found in body")
+
+    def test_logout(self):
+        response = self.post_data()
+        self.assertEqual(status.HTTP_201_CREATED, response.status_code)
+
+        data = {
+            "username": self.username,
+            "password": self.password1
+        }
+
+        request = self.factory.post(self.login_url, data)
+        response = self.login_view(request)
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        auth_headers = {
+            'HTTP_AUTHORIZATION': 'Bearer ' + response.data.get('access')
+        }
+
+        request = self.factory.post(self.logout_url, **auth_headers)
+        response = self.logout_view(request)
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+
